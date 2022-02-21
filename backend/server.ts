2f26813d2ab582f5f07632b505c1b7ac4fcb103f
HaseFlrn <florianhase12@gmail.com>
2022-02-21 16:21:53 +0100

    fixed workflow-errors

--- backend/server.ts
+++ backend/server.ts
@@ -3,14 +3,14 @@ import { Request } from 'https://deno.land/x/request@1.3.2/mod.ts';
 
 const app = opine();
 
-const pathToIndexHTMLFile = `${Deno.cwd()}/frontend/index.html`;
+const pathToIndexHTMLFile = `${Deno.cwd().replace('backend','frontend/index.html') }`;
 
-app.get("/", function (req, res) {
+app.get("/", function (_req, res) {
     res.sendFile(pathToIndexHTMLFile); // consider providing the index.html via github pages as an alternative
 });
 
-app.get("/getISSPosition", async function (req, res) {
-    const result: any = await Request.get('http://api.open-notify.org/iss-now.json');
+app.get("/getISSPosition", async function (_req, res) {
+    const { result } = await Request.get('http://api.open-notify.org/iss-now.json');
 
     res.send(`the ISS is above: ${JSON.stringify(result.iss_position)}`);
 });
