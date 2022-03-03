028afc68b9e7f0ea1dc7619ace745594e92a3f77
HaseFlrn <florianhase12@gmail.com>
2022-03-03 16:38:19 +0100

    Youtube OAuth hinzugefügt

--- backend/src/api/routes/youtube_router.ts
+++ backend/src/api/routes/youtube_router.ts
@@ -1,11 +1,21 @@
 import { Router } from "https://deno.land/x/opine@2.1.1/mod.ts";
+import OAuth2Client from "../connections/YoutubeClient.ts";
 
 const router = Router();
 
 router
-	.get("/", (_req, res) => {
-		res.send("Hello Youtube!");
-	});
+	.get("/:token", (req, res) => {
+		res.send(`Hello Youtube! ${req.params.token}`);
+	})
+	.get("/login", (_req, res) => {
+		res.redirect(OAuth2Client.code.getAuthorizationUri().toString());
+	})
+	.get("/auth/callback", async (req, res) => {
+		const code = req.originalUrl;
+		const token = await OAuth2Client.code.getToken(code);
+		
+		res.redirect("https://localhost:3000/api/v1/youtube/" + token.accessToken)
+	})
 //.get("/PATH", (req,res) => {function})
 
 export default router;
