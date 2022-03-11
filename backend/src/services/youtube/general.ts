49a082bbfe70c7e13f346fb4cc7cc12480015cab
TobiasFitzke <84978037+TobiasFitzke01@users.noreply.github.com>
2022-03-11 20:26:25 +0100

    get name/description/publishedAt functions

--- backend/src/services/youtube/general.ts
+++ backend/src/services/youtube/general.ts
@@ -1,6 +1,23 @@
-export async function getChannelName(token:String) {
+export async function getChannelInformations(token:String) {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&mine=true&access_token=${token}`);
     const res = await response.json();
+    return res; 
+}
+
+export async function getChannelName(token:String) {
+    const res = await getChannelInformations(token);
     const channelName = res.items[0].snippet.title;
     return channelName; 
+}
+
+export async function getChannelDescription(token:String) {
+    const res = await getChannelInformations(token);
+    const channelDescription = res.items[0].snippet.description;
+    return channelDescription; 
+}
+
+export async function getChannelPublishedAt(token:String) {
+    const res = await getChannelInformations(token);
+    const channelPublishedAt = res.items[0].snippet.publishedAt;
+    return channelPublishedAt; 
 }
