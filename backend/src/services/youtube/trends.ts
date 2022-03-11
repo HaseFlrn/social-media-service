c7af49a4178b697daa420855dddd972d15337d41
HaseFlrn <72435707+HaseFlrn@users.noreply.github.com>
2022-03-11 20:31:45 +0000

    Deploying to deploy from @ HaseFlrn/social-media-service@69a28a868b0654f3a6c0ed3ccac48f494e2644d2 🚀

--- backend/src/services/youtube/trends.ts
+++
@@ -1,7 +0,0 @@
-// NOTES:
-// curl \
-//   'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&part=snippet&chart=mostPopular&key=[YOUR_API_KEY]' \
-//   --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-//   --header 'Accept: application/json' \
-//   --compressed
-// => Top 200 videos of youtube
--- social-media-backend/src/main.js
+++
@@ -1,6 +0,0 @@
-import server from "./server.svelte";
-
-const server = new server({
-	target: document.querySelector("#__snel"),
-	props: {},
-});
