7ce7f881b97e0befa1e3151c9630bff70c9d54c9
TobiasFitzke <84978037+TobiasFitzke01@users.noreply.github.com>
2022-03-11 20:57:35 +0100

    error fix

--- backend/src/services/youtube/general.ts
+++ backend/src/services/youtube/general.ts
@@ -1,22 +1,22 @@
-export async function getChannelInformations(token: String) {
+export async function getChannelInformations(token:string) {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&mine=true&access_token=${token}`);
     const res = await response.json();
     return res; 
 }
 
-export async function getChannelName(token:String) {
+export async function getChannelName(token:string) {
     const res = await getChannelInformations(token);
     const channelName = res.items[0].snippet.title;
     return channelName; 
 }
 
-export async function getChannelDescription(token:String) {
+export async function getChannelDescription(token:string) {
     const res = await getChannelInformations(token);
     const channelDescription = res.items[0].snippet.description;
     return channelDescription; 
 }
 
-export async function getChannelPublishedAt(token:String) {
+export async function getChannelPublishedAt(token:string) {
     const res = await getChannelInformations(token);
     const channelPublishedAt = res.items[0].snippet.publishedAt;
     return channelPublishedAt; 
--- backend/src/services/youtube/myStats.ts
+++ backend/src/services/youtube/myStats.ts
@@ -1,26 +1,26 @@
 import {getChannelInformations} from './general.ts';
  
 //Channel Stats
-export async function getVideoQuantity(token:String) {
+export async function getVideoQuantity(token:string) {
     const res = await getChannelInformations(token);
     const videoQuantity = res.items[0].statistics.videoCount;
     return videoQuantity; 
 }
 
-export async function getSubscriberQuantity(token:String) {
+export async function getSubscriberQuantity(token:string) {
     const res = await getChannelInformations(token);
     const subscriberQuantity = res.items[0].statistics.subscriberCount;
     return subscriberQuantity; 
 }
 
-export async function getAllTimeViews(token:String) {
+export async function getAllTimeViews(token:string) {
     const res = await getChannelInformations(token);
     const allTimeViews = res.items[0].statistics.viewCount;
     return allTimeViews; 
 }
 
 //Latest Video
-export async function getLatestVideo(token: String) {
+export async function getLatestVideo(token:string) {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&maxResults=25&mine=true&access_token=${token}`);
     const res = await response.json();
     const latestVideo = res.items[0].contentDetails.upload.videoId;
@@ -28,31 +28,31 @@ export async function getLatestVideo(token: String) {
 }
 
 //Video Stats
-export async function getVideoStatistics(token: String, videoId: String) {
+export async function getVideoStatistics(token:string, videoId:string) {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&access_token=${token}`);
     const res = await response.json();
     return res; 
 }
 
-export async function getVideoViewsQuantity(token:String, videoId: String) {
+export async function getVideoViewsQuantity(token:string, videoId:string) {
     const res = await getVideoStatistics(token, videoId);
     const videoViews = res.items[0].statistics.viewCount;
     return videoViews; 
 }
 
-export async function getVideoLikesQuantity(token:String, videoId: String) {
+export async function getVideoLikesQuantity(token:string, videoId:string) {
     const res = await getVideoStatistics(token, videoId);
     const videoLikes = res.items[0].statistics.likeCount;
     return videoLikes; 
 }
 
-export async function getVideoDislikesQuantity(token:String, videoId: String) {
+export async function getVideoDislikesQuantity(token:string, videoId:string) {
     const res = await getVideoStatistics(token, videoId);
     const videoDislikes = res.items[0].statistics.dislikeCount;
     return videoDislikes; 
 }
 
-export async function getVideoCommentQuantity(token:String, videoId: String) {
+export async function getVideoCommentQuantity(token:string, videoId:string) {
     const res = await getVideoStatistics(token, videoId);
     const videoComments = res.items[0].statistics.commentCount;
     return videoComments; 
