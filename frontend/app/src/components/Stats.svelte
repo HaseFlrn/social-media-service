d505178eb6a7b75f4300d27cc1093f00d9ec3636
Matheis <lea.matheis@sap.com>
2022-03-14 09:35:54 +0100

    small change

--- frontend/app/src/components/Stats.svelte
+++ frontend/app/src/components/Stats.svelte
@@ -1,9 +1,20 @@
 <script>
+  import LayoutGrid from "/node_modules/@smui/layout-grid";
 </script>
 
 <main>
   <h1>My Stats</h1>
+
+  <h3> Overview </h3>
+
+
+  <h3>Views</h3>
+  <h3>Likes</h3>
+  <h3>Uploads</h3>
+  <h3>Comments</h3>
+
 </main>
 
 <style>
+
 </style>
