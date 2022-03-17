d3f2ce3b6d3e0f77fc5678c28f91cc947001600f
MatthiasBiermanns <M.Biermanns02@gmail.com>
2022-03-17 14:23:47 +0100

    start subscription services

--- backend/src/services/youtube/mySubscriptions.ts
+++ backend/src/services/youtube/mySubscriptions.ts
@@ -5,11 +5,22 @@
 
 //returns all channels, that the current Channels subscribes
 export async function getAllSubscriptions(token: string) {
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&access_token=${token}`);
-  const res = await response.json();
-  
-  console.log(res);
-  return res;
+  const url = `https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&access_token=${token}`;
+  let finalResult;
+  let totalResults: number;
+  let readResults = 0;
+  let pageToken = '';
+  do {
+    const response = await fetch((pageToken == '') ? url : (url + '&page_token=' + pageToken));
+    const res = await response.json();
+
+    finalResult = finalResult + res['items'];
+
+    totalResults = res['pageInfo']['totalResults'];
+    readResults = readResults + res['pageInfo']['resultsPerPage'];
+    pageToken = res['nextPageToken'];
+  } while(readResults < totalResults) 
+  return finalResult;
 }
 
 
