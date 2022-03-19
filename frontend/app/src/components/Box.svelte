fe0224dc55815946e74f541668e6b080dabdccf7
RebekkaMiguez <rebekka.miguez@gmx.de>
2022-03-20 00:45:40 +0100

    dashboard basic set-up

---
+++ frontend/app/src/components/Box.svelte
@@ -0,0 +1,16 @@
+<div class="box">
+	<slot></slot>
+</div>
+
+<style>
+	.box {
+        float: left;
+		width: 100%;
+        height: auto;
+		border: 1px solid #eb7d00;
+		border-radius: 2px;
+        box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 20px;
+		margin: 1em 1em 1em 0;
+	}
+</style>
--- frontend/app/src/components/Dashboard.svelte
+++ frontend/app/src/components/Dashboard.svelte
@@ -1,9 +1,39 @@
 <script>
+  import Box from './Box.svelte';
 </script>
 
 <main>
   <h1>Dashboard</h1>
+  <div class="flex-container">
+    <Box>
+      <h2><a style="color: #eb7d00" href="/mystats">My Stats</a></h2>
+      <iframe width= 100% height= 400 src="/mystats" title="my stats"/>
+    </Box>
+
+    <Box>
+      <h2><a style="color: #eb7d00" href="/mysubs">My Subscriptions</a></h2>
+      <iframe width= 100% height= 400 src="/mysubs" title="my subs" />
+    </Box>
+
+    <Box>
+      <h2><a style="color: #eb7d00" href="/trends">Trends</a></h2>
+      <iframe width= 100% height= 400 src="/trends" title="trends" />
+    </Box>
+  </div>
+
 </main>
 
 <style>
+  h1 {
+    font-size: 50px;
+    color:#eb7d00;
+    text-align: center;
+  }
+
+  .flex-container {
+    display: flex;
+    justify-content: center;
+  }
+
+
 </style>
