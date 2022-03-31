77bf59efde7e431282286646ed825fc1b84e8810
HaseFlrn <florianhase12@gmail.com>
2022-03-31 17:03:22 +0200

    added SSL_PATH to .env

--- backend/server.ts
+++ backend/server.ts
@@ -2,10 +2,11 @@ import { Application, Router, oakCors, config } from "./deps.ts";
 
 import Routes from "./src/api/routes/router.ts";
 
-const { PORT } = config();
+const { PORT, SSL_PATH } = config();
 const port = parseInt(PORT);
 const app = new Application();
 const router = new Router();
+let path = "";
 
 if(Number.isNaN(port)){
 	Deno.exit(1);
@@ -16,8 +17,12 @@ router.use("/api/v1", Routes.routes(), Routes.allowedMethods());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const certFile = "myCertFile.cert";
-const keyFile = "myKeyFile.key";
+if(SSL_PATH){
+	path = SSL_PATH;
+}
+
+const certFile = `${path}certificate.cert`;
+const keyFile = `${path}private.key`;
 
 
 app.listen({
