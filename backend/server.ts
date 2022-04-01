51edff62d5596d795359a25bf99d8dd0716ee859
HaseFlrn <florianhase12@gmail.com>
2022-04-01 17:03:16 +0200

    added 404

--- backend/server.ts
+++ backend/server.ts
@@ -13,6 +13,10 @@ if(Number.isNaN(port)){
 }
 
 app.use( oakCors( { origin: "*" } ) );
+router.get("/(.*)", (ctx: any) => {      
+    ctx.response.status = 404;
+    ctx.response.body = "404 | Page not Found";
+});
 router.use("/api/v1", Routes.routes(), Routes.allowedMethods());
 app.use(router.routes());
 app.use(router.allowedMethods());
