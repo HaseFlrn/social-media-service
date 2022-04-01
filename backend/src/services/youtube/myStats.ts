6a8bc27c49ee1f3c69015c910f67df3fbc28935d
HaseFlrn <florianhase12@gmail.com>
2022-04-01 16:50:41 +0200

    try again

--- backend/src/services/youtube/myStats.ts
+++ backend/src/services/youtube/myStats.ts
@@ -154,7 +154,7 @@ export default class myStats{
         try{
             const response = await fetch(`${myStats.playlistsUrl}?part=snippet%2CcontentDetails&mine=true&access_token=${req.token}`);
             const data = await response.json();
-            
+
             if(data.items.length != 0){
                 // deno-lint-ignore ban-types
                 const finalResult: {latestPlaylist: String, allplaylist: String[]} = { 
@@ -254,7 +254,7 @@ export default class myStats{
                 const response = await fetch(`${myStats.reportsUrl}?endDate=${tempEndDate}&ids=channel%3D%3DMINE&metrics=views%2Ccomments%2Clikes%2Cdislikes%2CestimatedMinutesWatched%2CaverageViewDuration&startDate=${tempStartDate}&access_token=${req.token}`);
                 const data = await response.json();
 
-                if(data.rows.length != 0){
+                if(data.rows && data.rows.length != 0){
                     const oneMonthStats = { 
                         views: data.rows[0][0],
                         comments: data.rows[0][1],
